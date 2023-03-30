d76f450c80ee44ec15d9a409fe2554ecb45a4665
Replit user <>
2023-03-30 16:16:13 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -36,7 +36,7 @@ void append(ArrayList * l, void * data){
 
 void push(ArrayList * l, void * data, int i){
 
-  if ( i > l->size);
+  if ( i > l->size) return;
     
   if( l->capacity > l->size){
 
